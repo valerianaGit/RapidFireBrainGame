389635d22420af65d2ecfb1affd3ff83231c37e1
vdr <valeria.do@gmail.com>
2024-06-10 10:20:46 -0500

    work in progress

--- lib/widgets/shake_detector.dart
+++ lib/widgets/shake_detector.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:async'; // for stream subscription
+//import 'package:flutter/services.dart';
 
 class ShakeDetector extends StatefulWidget {
   final Function() onShake;
@@ -13,24 +15,60 @@ class ShakeDetector extends StatefulWidget {
 class _ShakeDetectorState extends State<ShakeDetector> {
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
   DateTime? _lastShakeTime;
-  static const shakeThreshold = 15;
-  static const shakeCooldown = Duration(milliseconds: 500);
+  static const _shakeThreshold = 15;
+  static const _shakeCooldown = Duration(milliseconds: 500);
+  UserAccelerometerEvent? _userAccelerometerEvent;
+  Duration sensorInterval = SensorInterval.normalInterval;
+  DateTime? _userAccelerometerUpdateTime;
+  int? _userAccelerometerLastInterval;
 
   @override
   void initState() {
     super.initState();
     _streamSubscriptions.add(
-      accelerometerEvents.listen((AccelerometerEvent event) {
-        final totalAcceleration = event.x.abs() + event.y.abs() + event.z.abs();
-        final currentTime = DateTime.now();
-
-        if (totalAcceleration > shakeThreshold && 
-            (_lastShakeTime == null || currentTime.difference(_lastShakeTime!) > shakeCooldown)) {
-          _lastShakeTime = currentTime;
+      userAccelerometerEventStream(samplingPeriod: sensorInterval).listen(
+        (UserAccelerometerEvent event) {
+          final totalAcceleration = event.x.abs() + event.y.abs() + event.z.abs();
+          final now = DateTime.now();
+          setState(() {
+             _userAccelerometerEvent = event;
+                   if (totalAcceleration > _shakeThreshold &&
+            (_lastShakeTime == null || now.difference(_lastShakeTime!) > _shakeCooldown)) {
+          _lastShakeTime = now;
           widget.onShake();
-        }
-      }),
+        },
+  
+           
+        //     if (_userAccelerometerUpdateTime != null) {
+        //       final interval = now.difference(_userAccelerometerUpdateTime!);
+        //       if (interval > _shakeCooldown) {
+        //         _userAccelerometerLastInterval = interval.inMilliseconds;
+        //       }
+        //     }
+        //   });
+        //   _userAccelerometerUpdateTime = now;
+        //   widget.onShake;
+  
+
+ 
+        // },
+        onError: (e) {
+          //MARK: Next release, handle so it goes to next move and doesn't use shake -take shake out of the running
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support shaking. Do a dance instead"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
     );
+
+
   }
 
   @override
@@ -43,6 +81,6 @@ class _ShakeDetectorState extends State<ShakeDetector> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(); // Empty container, to overlay on top of the shakedetector container 
+    return Container(); // Empty container, to overlay on top of the shakedetector container
   }
 }
